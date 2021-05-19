e5dbf5ce67dcb2ee35f28502d327f2b38c4d1fd3
KITO <kiyj585@gmail.com>
2021-05-20 01:24:09 +0300

    add 1-5 lab

---
+++ LAB1_RTOS/Objects/drivers.d
@@ -0,0 +1,15 @@
+.\objects\drivers.o: drivers.c
+.\objects\drivers.o: drivers.h
+.\objects\drivers.o: C:\Users\Admin\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
+.\objects\drivers.o: .\RTE\_Target_1\RTE_Components.h
+.\objects\drivers.o: C:\Users\Admin\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm3.h
+.\objects\drivers.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\drivers.o: C:\Users\Admin\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\drivers.o: C:\Users\Admin\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\drivers.o: C:\Users\Admin\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\drivers.o: C:\Users\Admin\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
+.\objects\drivers.o: .\RTE\Device\STM32F103C8\stm32f10x_conf.h
+.\objects\drivers.o: C:\Users\Admin\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h
+.\objects\drivers.o: C:\Users\Admin\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
+.\objects\drivers.o: C:\Users\Admin\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h
+.\objects\drivers.o: C:\Users\Admin\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\misc.h
---
+++ LAB1_RTOS/structures.h
@@ -0,0 +1,12 @@
+#include "stm32f10x.h"
+
+void initGPIOStructures(void);
+
+void PWMStructures(uint32_t GPIOAWorkTime, uint32_t GPIOBWorkTime, uint32_t period);
+
+void _delay_ms_str(uint32_t ms);
+
+#define DELAY1 9550 
+#define DELAY2 2550 
+#define DELAY3 3550 
+#define DELAY4 4550 
